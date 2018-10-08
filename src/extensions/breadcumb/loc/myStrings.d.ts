f5e281adfc3663377fcf1460f259d3c0f30b52af
Americo Perez <americo@americospdev.onmicrosoft.com>
2018-10-08 10:52:34 +0200

    Start

---
+++ src/extensions/breadcumb/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IBreadcumbApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'BreadcumbApplicationCustomizerStrings' {
+  const strings: IBreadcumbApplicationCustomizerStrings;
+  export = strings;
+}
